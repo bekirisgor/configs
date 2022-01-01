e539b678be73c97a8ef8de8e63cdd2109459a18e
bekirisgor <bekirisgor@gmail.com>
2022-01-01 22:18:08 +0300

    tide and font added

--- fish/config.fish
+++ fish/config.fish
@@ -13,9 +13,9 @@ end
 
 if command -v exa > /dev/null
 	abbr -a l 'exa'
-	abbr -a ls 'exa'
-	abbr -a ll 'exa -l'
-	abbr -a lll 'exa -la'
+	abbr -a ls 'exa --icons'
+	abbr -a ll 'exa -l --icons'
+	abbr -a lll 'exa -la --icons'
 else
 	abbr -a l 'ls'
 	abbr -a ll 'ls -l'
