3701b1c267cc852294033b3436c4571cd8fe87c1
bekirisgor <bekirisgor@gmail.com>
2022-12-07 12:08:24 +0300

    chore(bufferline): Update latest tag 3.*

--- nvim/lua/plugins/setup.lua
+++ nvim/lua/plugins/setup.lua
@@ -19,7 +19,7 @@ return require("packer").startup(function(use)
 		"nvim-lualine/lualine.nvim",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
 	})
-	use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" })
+	use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "kyazdani42/nvim-web-devicons" })
 	use("mhinz/vim-startify")
 	use("xiyaowong/nvim-transparent")
 
